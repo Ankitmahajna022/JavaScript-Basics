8551b69e5a3b21dfe1d661c709442f172639feb3
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-01-18 12:29:01 +0530

    Add files via upload

---
+++ JavaScript Basics/Question-16.js
@@ -0,0 +1,7 @@
+//Declare a variable age and assign it your age. Then, change the value of age to 5 years older. Print both the original and updated values.
+
+let age=20;
+
+age=age+5;
+
+console.log(age);
